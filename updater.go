784a89dded6712630a3e5b88ad6eb88ba3ba8c92
hidez8891 <427357+hidez8891@users.noreply.github.com>
2016-09-15 22:10:38 +0900

    add method: zip.Updater can add file

--- updater.go
+++ updater.go
@@ -5,15 +5,24 @@
 package zip
 
 import (
+	"fmt"
+	"io"
 	"io/ioutil"
 	"os"
 )
+
+type editorFileHeader struct {
+	*FileHeader
+	isNewFile bool
+}
 
 // Updater implements a zip file edit-writer.
 type Updater struct {
 	path    string
 	r       *ReadCloser
-	File    []*File
+	w       *Writer
+	tmpw    *os.File
+	File    []*editorFileHeader
 	Comment string
 }
 
@@ -27,11 +36,71 @@ func OpenUpdater(path string) (*Updater, error) {
 	u := &Updater{
 		path:    path,
 		r:       r,
-		File:    r.File,
 		Comment: r.Comment,
 	}
 
+	u.File = make([]*editorFileHeader, 0)
+	for _, file := range r.File {
+		u.File = append(u.File, &editorFileHeader{
+			FileHeader: &file.FileHeader,
+			isNewFile:  false,
+		})
+	}
+
 	return u, nil
+}
+
+// AppendFile add a file to the zip archive at last.
+func (u *Updater) AppendFile(name string, streamMode bool) (io.Writer, error) {
+	writer, err := u.createWriter(name, streamMode)
+	if err != nil {
+		return nil, err
+	}
+
+	u.File = append(u.File, &editorFileHeader{
+		FileHeader: u.w.dir[len(u.w.dir)-1].FileHeader,
+		isNewFile:  true,
+	})
+
+	return writer, nil
+}
+
+// InsertFile add a file to the zip archive at pos-th.
+func (u *Updater) InsertFile(pos int, name string, streamMode bool) (io.Writer, error) {
+	if len(u.File) >= pos {
+		return nil, fmt.Errorf("zip: []File index out of range")
+	}
+
+	writer, err := u.createWriter(name, streamMode)
+	if err != nil {
+		return nil, err
+	}
+
+	u.File = append(u.File, nil)
+	copy(u.File[pos+1:], u.File[pos:])
+	u.File[pos] = &editorFileHeader{
+		FileHeader: u.w.dir[len(u.w.dir)-1].FileHeader,
+		isNewFile:  true,
+	}
+
+	return writer, nil
+}
+
+func (u *Updater) createWriter(name string, streamMode bool) (io.Writer, error) {
+	if u.w == nil {
+		var err error
+		if u.tmpw, err = ioutil.TempFile("", "tmp_zip_updater"); err != nil {
+			return nil, err
+		}
+		u.w = NewWriter(u.tmpw)
+	}
+
+	writer, err := u.w.Create(name, streamMode)
+	if err != nil {
+		return nil, err
+	}
+
+	return writer, nil
 }
 
 // SaveAs write all changes to new zip file and close file.
@@ -47,9 +116,59 @@ func (u *Updater) SaveAs(newpath string) error {
 		}
 	}()
 
+	// reopen temporary file
+	var tmpreader *Reader
+	if u.w != nil {
+		var err error
+		if err = u.w.Close(); err != nil {
+			return err
+		}
+		u.w = nil
+
+		state, err := u.tmpw.Stat()
+		if err != nil {
+			return err
+		}
+
+		if _, err = u.tmpw.Seek(0, os.SEEK_SET); err != nil {
+			return err
+		}
+
+		tmpreader, err = NewReader(u.tmpw, state.Size())
+		if err != nil {
+			return err
+		}
+	}
+
 	// copy & write
 	w := NewWriter(newfile)
-	for _, file := range u.File {
+	for _, header := range u.File {
+		var (
+			file *File
+			zipr *Reader
+		)
+
+		if header.isNewFile {
+			zipr = tmpreader
+		} else {
+			zipr = &u.r.Reader
+		}
+
+		if zipr == nil {
+			return fmt.Errorf("zip: zip reader has null pointer")
+		}
+
+		for _, f := range zipr.File {
+			if f.Name == header.Name {
+				file = f
+				break
+			}
+		}
+
+		if file == nil {
+			return fmt.Errorf("zip: file %s does not exist", file.Name)
+		}
+
 		if err := w.addFile(file); err != nil {
 			newfile.Close()
 			return err
@@ -64,10 +183,18 @@ func (u *Updater) SaveAs(newpath string) error {
 		return err
 	}
 	newfile = nil
+
 	if err := u.r.Close(); err != nil {
 		return err
 	}
 	u.r = nil
+
+	if u.tmpw != nil {
+		if err := u.tmpw.Close(); err != nil {
+			return err
+		}
+		u.tmpw = nil
+	}
 
 	return nil
 }
--- updater_test.go
+++ updater_test.go
@@ -13,13 +13,33 @@ import (
 	"testing"
 )
 
-type UpdateTest struct {
-	Name string
+var updateTests = []ZipTest{
+	{
+		Name:    "test.zip",
+		Comment: "This is a zipfile comment.",
+		File: []ZipTestFile{
+			{
+				Name:    "test.txt",
+				Content: []byte("This is a test text file.\n"),
+				Mtime:   "09-05-10 12:12:02",
+				Mode:    0644,
+			},
+			{
+				Name:  "gophercolor16x16.png",
+				File:  "gophercolor16x16.png",
+				Mtime: "09-05-10 15:52:58",
+				Mode:  0644,
+			},
+		},
+	},
 }
 
-var updateTests = []UpdateTest{
+var updateAppendFiles = []WriteTest{
 	{
-		Name: "test.zip",
+		Name:   "foo",
+		Data:   []byte("Rabbits, guinea pigs, gophers, marsupial rats, and quolls."),
+		Method: Store,
+		Mode:   0666,
 	},
 }
 
@@ -29,7 +49,7 @@ func TestUpdaterOnlyCopy(t *testing.T) {
 	}
 }
 
-func updaterOnlyCopy(t *testing.T, zt UpdateTest) {
+func updaterOnlyCopy(t *testing.T, zt ZipTest) {
 	testfile := filepath.Join("testdata", zt.Name)
 	z, err := OpenUpdater(testfile)
 	if err != nil {
@@ -113,6 +133,74 @@ func sameReader(r1, r2 io.Reader) bool {
 
 		if bytes.Equal(b1, b2) == false {
 			return false
+		}
+	}
+}
+
+func TestUpdaterAppendFile(t *testing.T) {
+	for _, zt := range updateTests {
+		updaterAppendFile(t, zt)
+	}
+}
+
+func updaterAppendFile(t *testing.T, zt ZipTest) {
+	testfile := filepath.Join("testdata", zt.Name)
+	z, err := OpenUpdater(testfile)
+	if err != nil {
+		t.Fatalf("%s open failed: %v", zt.Name, err)
+	}
+
+	for _, file := range updateAppendFiles {
+		w, err := z.AppendFile(file.Name, true)
+		if err != nil {
+			t.Fatalf("%s failed append file header: %v", zt.Name, err)
+		}
+		if _, err := io.Copy(w, bytes.NewReader(file.Data)); err != nil {
+			t.Fatalf("%s failed append file data: %v", zt.Name, err)
+		}
+	}
+
+	tmpfile, err := ioutil.TempFile("", "test_zip_updater_")
+	if err != nil {
+		t.Fatalf("tempfile create failed: %v", err)
+	}
+	tmpfile.Close()
+	defer os.Remove(tmpfile.Name())
+
+	if err := z.SaveAs(tmpfile.Name()); err != nil {
+		t.Fatalf("%s save to %s failed: %v", zt.Name, tmpfile.Name(), err)
+	}
+
+	r, err := OpenReader(tmpfile.Name())
+	if err != nil {
+		t.Fatalf("%s open failed: %v", tmpfile.Name(), err)
+	}
+
+	for _, file := range zt.File {
+		var found bool
+		for _, f := range r.File {
+			if file.Name == f.Name {
+				found = true
+				break
+			}
+		}
+
+		if found == false {
+			t.Fatalf("%s does not have %s", tmpfile.Name(), file.Name)
+		}
+	}
+
+	for _, file := range updateAppendFiles {
+		var found bool
+		for _, f := range r.File {
+			if file.Name == f.Name {
+				found = true
+				break
+			}
+		}
+
+		if found == false {
+			t.Fatalf("%s does not have %s", tmpfile.Name(), file.Name)
 		}
 	}
 }
