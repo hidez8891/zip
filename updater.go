b39b82fbb7dcad62d3c953af6e502256957c9b64
hidez8891 <427357+hidez8891@users.noreply.github.com>
2018-09-12 22:53:12 +0900

    WIP: add Updater.Create and Update

--- updater.go
+++ updater.go
@@ -5,15 +5,33 @@
 package zip
 
 import (
+	"bytes"
 	"errors"
 	"io"
 )
+
+// A WriteCloser implements the io.WriteCloser
+type WriteCloser struct {
+	writer io.Writer
+	closer io.Closer
+}
+
+// Write implements the io.WriteCloser interface.
+func (w *WriteCloser) Write(p []byte) (int, error) {
+	return w.writer.Write(p)
+}
+
+// Close implements the io.WriteCloser interface.
+func (w *WriteCloser) Close() error {
+	return w.closer.Close()
+}
 
 // Updater provides editing of zip files.
 type Updater struct {
 	path    string
 	files   []string
 	headers map[string]*FileHeader
+	entries map[string]*bytes.Buffer
 	r       *ReadCloser
 }
 
@@ -35,6 +53,7 @@ func NewUpdater(path string) (*Updater, error) {
 		path:    path,
 		files:   files,
 		headers: headers,
+		entries: make(map[string]*bytes.Buffer),
 		r:       r,
 	}, nil
 }
@@ -54,6 +73,15 @@ func (u *Updater) Open(name string) (io.ReadCloser, error) {
 		return nil, errors.New("File not found")
 	}
 
+	if buf, ok := u.entries[name]; ok {
+		b := buf.Bytes()
+		z, err := NewReader(bytes.NewReader(b), int64(len(b)))
+		if err != nil {
+			return nil, err
+		}
+		return z.File[0].Open()
+	}
+
 	for _, zf := range u.r.File {
 		if zf.Name == name {
 			return zf.Open()
@@ -63,13 +91,48 @@ func (u *Updater) Open(name string) (io.ReadCloser, error) {
 }
 
 // Create returns a Writer to which the file contents should be written.
-func (u *Updater) Create(name string) (io.Writer, error) {
-	return nil, errors.New("internal error: Unimplemented")
+func (u *Updater) Create(name string) (io.WriteCloser, error) {
+	if _, ok := u.headers[name]; ok {
+		return nil, errors.New("invalid duplicate file name")
+	}
+
+	u.entries[name] = new(bytes.Buffer)
+	z := NewWriter(u.entries[name])
+
+	w, err := z.Create(name)
+	if err != nil {
+		return nil, err
+	}
+	u.files = append(u.files, name)
+	u.headers[name] = z.dir[0].FileHeader
+
+	wc := &WriteCloser{
+		writer: w,
+		closer: z,
+	}
+	return wc, nil
 }
 
 // Update returns a Writer to which the file contents should be overwritten.
-func (u *Updater) Update(name string) (io.Writer, error) {
-	return nil, errors.New("internal error: Unimplemented")
+func (u *Updater) Update(name string) (io.WriteCloser, error) {
+	if _, ok := u.headers[name]; !ok {
+		return nil, errors.New("not found file name")
+	}
+
+	u.entries[name] = new(bytes.Buffer)
+	z := NewWriter(u.entries[name])
+
+	w, err := z.CreateHeader(u.headers[name])
+	if err != nil {
+		return nil, err
+	}
+	u.headers[name] = z.dir[0].FileHeader
+
+	wc := &WriteCloser{
+		writer: w,
+		closer: z,
+	}
+	return wc, nil
 }
 
 // Save saves the changes and ends editing.
@@ -78,6 +141,17 @@ func (u *Updater) Save() error {
 	if err != nil {
 		return err
 	}
+
+	// tempfile 作成
+	// foreach files
+	//   書き込み領域を取得
+	//   ファイルを書き込み
+	// tempfile.close
+	//
+	// u.path を old に rename
+	// tempfile を u.path に rename
+	// old を削除
+
 	return errors.New("internal error: Unimplemented")
 }
 
@@ -87,11 +161,21 @@ func (u *Updater) SaveAs(path string) error {
 	if err != nil {
 		return err
 	}
+
+	// path 作成
+	// foreach files
+	//   書き込み領域を取得
+	//   ファイルを書き込み
+	// path.close
+
 	return errors.New("internal error: Unimplemented")
 }
 
 // Cancel discards the changes and ends editing.
 func (u *Updater) Cancel() error {
+	u.files = make([]string, 0)
+	u.headers = make(map[string]*FileHeader, 0)
+	u.entries = make(map[string]*bytes.Buffer, 0)
 	return u.r.Close()
 }
 
--- updater_test.go
+++ updater_test.go
@@ -10,17 +10,31 @@ import (
 	"testing"
 )
 
-func TestUpdaterOnlyRead(t *testing.T) {
-	var testcase []ZipTestFile
-	file := "winxp.zip"
-	for _, tt := range tests {
-		if tt.Name == file {
-			testcase = tt.File
-			break
-		}
-	}
+var updateTest = ZipTest{
+	Name: "winxp.zip",
+	File: []ZipTestFile{
+		{
+			Name:    "hello",
+			Content: []byte("world \r\n"),
+		},
+		{
+			Name:    "dir/bar",
+			Content: []byte("foo \r\n"),
+		},
+		{
+			Name:    "dir/empty/",
+			Content: []byte{},
+		},
+		{
+			Name:    "readonly",
+			Content: []byte("important \r\n"),
+		},
+	},
+}
 
-	z, err := NewUpdater("testdata/" + file)
+func TestUpdaterOnlyRead(t *testing.T) {
+	testcase := updateTest.File
+	z, err := NewUpdater("testdata/" + updateTest.Name)
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -28,33 +42,136 @@ func TestUpdaterOnlyRead(t *testing.T) {
 
 	files := z.Files()
 	if len(testcase) != len(files) {
-		t.Fatalf("file count=%d, want %d", len(testcase), len(files))
+		t.Fatalf("file count=%d, want %d", len(files), len(testcase))
 	}
-	for i, tc := range testcase {
-		if tc.Name != files[i].Name {
-			t.Errorf("name=%q, want %q", tc.Name, files[i].Name)
+	for i, ztf := range testcase {
+		if files[i].Name != ztf.Name {
+			t.Errorf("name=%q, want %q", files[i].Name, ztf.Name)
 		}
+		compareContents(t, z, ztf)
+	}
+}
 
-		r, err := z.Open(tc.Name)
-		if err != nil {
-			t.Fatal(err)
-		}
+func TestUpdaterAddFile(t *testing.T) {
+	testcase := updateTest.File
+	testfile := ZipTestFile{
+		Name:    "test",
+		Content: []byte("text string"),
+	}
 
-		var b bytes.Buffer
-		_, err = io.Copy(&b, r)
-		r.Close()
-		if err != nil {
-			t.Fatal(err)
-		}
+	z, err := NewUpdater("testdata/" + updateTest.Name)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer z.Close()
 
-		buf := b.Bytes()
-		if len(buf) != len(tc.Content) {
-			t.Fatalf("filesize len=%d, want %d", len(buf), len(tc.Content))
+	// add file
+	wc, err := z.Create(testfile.Name)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	n, err := wc.Write(testfile.Content)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if n != len(testfile.Content) {
+		t.Fatalf("write size=%d, want %d", n, len(testfile.Content))
+	}
+	if err := wc.Close(); err != nil {
+		t.Fatal(err)
+	}
+
+	// check file
+	files := z.Files()
+	if len(files) != len(testcase)+1 {
+		t.Fatalf("file count=%d, want %d", len(files), len(testcase)+1)
+	}
+
+	for i, ztf := range testcase {
+		if files[i].Name != ztf.Name {
+			t.Errorf("name=%q, want %q", files[i].Name, ztf.Name)
 		}
-		for i, c := range tc.Content {
-			if c != buf[i] {
-				t.Fatalf("content[%d]=%q, want %q", i, c, buf[i])
-			}
+		compareContents(t, z, ztf)
+	}
+
+	last := files[len(files)-1]
+	if last.Name != testfile.Name {
+		t.Errorf("name=%q, want %q", last.Name, testfile.Name)
+	}
+	compareContents(t, z, testfile)
+}
+
+func TestUpdaterUpdateFile(t *testing.T) {
+	testcase := updateTest.File
+	testfile := ZipTestFile{
+		Name:    "dir/bar",
+		Content: []byte("update string"),
+	}
+
+	z, err := NewUpdater("testdata/" + updateTest.Name)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer z.Close()
+
+	// update file
+	wc, err := z.Update(testfile.Name)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	n, err := wc.Write(testfile.Content)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if n != len(testfile.Content) {
+		t.Fatalf("write size=%d, want %d", n, len(testfile.Content))
+	}
+	if err := wc.Close(); err != nil {
+		t.Fatal(err)
+	}
+
+	// check file
+	files := z.Files()
+	if len(files) != len(testcase) {
+		t.Fatalf("file count=%d, want %d", len(files), len(testcase))
+	}
+
+	for i, ztf := range testcase {
+		if files[i].Name != ztf.Name {
+			t.Errorf("name=%q, want %q", files[i].Name, ztf.Name)
+		}
+		if files[i].Name == testfile.Name {
+			compareContents(t, z, testfile)
+		} else {
+			compareContents(t, z, ztf)
+		}
+	}
+}
+
+func compareContents(t *testing.T, z *Updater, ztf ZipTestFile) {
+	t.Helper()
+
+	r, err := z.Open(ztf.Name)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	var b bytes.Buffer
+	_, err = io.Copy(&b, r)
+	r.Close()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	buf := b.Bytes()
+	if len(buf) != len(ztf.Content) {
+		t.Fatalf("filesize len=%d, want %d", len(buf), len(ztf.Content))
+	}
+	for i, c := range ztf.Content {
+		if buf[i] != c {
+			t.Fatalf("content[%d]=%q, want %q", i, buf[i], c)
 		}
 	}
 }
