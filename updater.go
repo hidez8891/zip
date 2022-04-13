100101a43be407088997444e2ab53230181563e5
hidez8891 <427357+hidez8891@users.noreply.github.com>
2022-04-13 21:29:57 +0900

    WIP: support file addition

--- updater.go
+++ updater.go
@@ -1,13 +1,17 @@
 package zip
 
 import (
+	"bytes"
 	"io"
 	"io/fs"
 )
 
 // Updater implements a zip file updater.
 type Updater struct {
-	zr *Reader
+	zr    *Reader
+	zw    *Writer
+	buf   *bytes.Buffer
+	files []fs.FileInfo
 }
 
 // NewUpdater returns a new Updater reading from r, which is assumed to
@@ -18,9 +22,17 @@ func NewUpdater(r io.ReaderAt, size int64) (*Updater, error) {
 		return nil, err
 	}
 
+	buf := new(bytes.Buffer)
+	zw := NewWriter(buf)
 	zu := &Updater{
-		zr: zr,
+		zr:  zr,
+		zw:  zw,
+		buf: buf,
 	}
+	if err := zu.initFiles(); err != nil {
+		return nil, err
+	}
+
 	return zu, nil
 }
 
@@ -30,8 +42,25 @@ func (z *Updater) Open(name string) (fs.File, error) {
 }
 
 // Create returns a WriteCloser to which the file contents should be written.
+// The file's contents must be written to the io.WriteCloser and closed
+// before the next call to Create, SaveAs, Discard or Close.
 func (z *Updater) Create(name string) (io.WriteCloser, error) {
-	return nil, nil
+	header := &FileHeader{
+		Name:   name,
+		Method: Deflate,
+	}
+
+	w, err := z.zw.CreateHeader(header)
+	if err != nil {
+		return nil, err
+	}
+
+	wc := &fileWriteCloser{
+		w:      w,
+		header: header,
+		parent: z,
+	}
+	return wc, nil
 }
 
 // Update returns a WriteCloser to which the file contents should be overwritten.
@@ -51,11 +80,58 @@ func (z *Updater) Delete(name string) error {
 
 // SaveAs saves the updated zip file to w.
 func (z *Updater) SaveAs(w io.Writer) error {
+	if err := z.zw.Close(); err != nil {
+		return err
+	}
+
+	ow := NewWriter(w)
+	for _, f := range z.zr.File {
+		header := f.FileHeader
+		fw, err := ow.CreateRaw(&header)
+		if err != nil {
+			return err
+		}
+		fr, err := f.OpenRaw()
+		if err != nil {
+			return err
+		}
+		io.Copy(fw, fr)
+	}
+
+	br, err := NewReader(bytes.NewReader(z.buf.Bytes()), int64(z.buf.Len()))
+	if err != nil {
+		return err
+	}
+	for _, f := range br.File {
+		header := f.FileHeader
+		fw, err := ow.CreateRaw(&header)
+		if err != nil {
+			return err
+		}
+		fr, err := f.OpenRaw()
+		if err != nil {
+			return err
+		}
+		_, err = io.Copy(fw, fr)
+		if err != nil {
+			return err
+		}
+	}
+
+	if err := ow.Close(); err != nil {
+		return err
+	}
 	return nil
 }
 
 // Discard discards the changes and ends editing.
 func (z *Updater) Discard() error {
+	z.zw.Close()
+
+	z.zr = nil
+	z.zw = nil
+	z.buf = nil
+	z.files = nil
 	return nil
 }
 
@@ -66,21 +142,45 @@ func (z *Updater) Close() error {
 
 // Files returns a list of file information in the zip file.
 func (z *Updater) Files() []fs.FileInfo {
-	info := make([]fs.FileInfo, len(z.zr.File))
+	return z.files[:]
+}
 
+func (z *Updater) initFiles() error {
+	z.zr.initFileList()
+
+	z.files = make([]fs.FileInfo, len(z.zr.File))
 	for i, f := range z.zr.File {
-		zf, err := z.zr.Open(f.Name)
-		if err != nil {
-			panic(err) // zip.Reader internal error
-		}
-
-		info[i], err = zf.Stat()
-		if err != nil {
-			panic(err) // zip.Reader internal error
-		}
-
-		zf.Close()
+		name := f.FileHeader.Name
+		e := z.zr.openLookup(name)
+		z.files[i] = e.stat()
 	}
+	return nil
+}
 
-	return info
+type fileWriteCloser struct {
+	w      io.Writer
+	header *FileHeader
+	parent *Updater
+}
+
+func (w *fileWriteCloser) Write(p []byte) (int, error) {
+	return w.w.Write(p)
+}
+
+func (w *fileWriteCloser) Close() error {
+	if fw, ok := w.w.(*fileWriter); ok {
+		if err := fw.close(); err != nil {
+			return err
+		}
+		info := headerFileInfo{w.header}
+		w.parent.files = append(w.parent.files, info)
+	} else {
+		info := &fileListEntry{
+			name:  w.header.Name,
+			file:  nil,
+			isDir: true,
+		}
+		w.parent.files = append(w.parent.files, info)
+	}
+	return nil
 }
--- updater_test.go
+++ updater_test.go
@@ -10,14 +10,16 @@ import (
 )
 
 type UpdaterTest struct {
-	Name string
-	File []ZipTestFile
+	Name       string
+	BaseFile   []ZipTestFile
+	AppendFile []WriteTest
+	ResultFile []ZipTestFile
 }
 
 var updateTests = []UpdaterTest{
 	{
 		Name: "test.zip",
-		File: []ZipTestFile{
+		BaseFile: []ZipTestFile{
 			{
 				Name:    "test.txt",
 				Content: []byte("This is a test text file.\n"),
@@ -25,6 +27,26 @@ var updateTests = []UpdaterTest{
 			{
 				Name: "gophercolor16x16.png",
 				File: "gophercolor16x16.png",
+			},
+		},
+		AppendFile: []WriteTest{
+			{
+				Name: "foo",
+				Data: []byte("Rabbits, guinea pigs, gophers, marsupial rats, and quolls."),
+			},
+		},
+		ResultFile: []ZipTestFile{
+			{
+				Name:    "test.txt",
+				Content: []byte("This is a test text file.\n"),
+			},
+			{
+				Name: "gophercolor16x16.png",
+				File: "gophercolor16x16.png",
+			},
+			{
+				Name:    "foo",
+				Content: []byte("Rabbits, guinea pigs, gophers, marsupial rats, and quolls."),
 			},
 		},
 	},
@@ -58,17 +80,35 @@ func updateTestZip(t *testing.T, zt UpdaterTest) {
 		return
 	}
 	defer zu.Discard()
-
-	if len(zu.Files()) != len(zt.File) {
-		t.Fatalf("file count=%d, want %d", len(zu.Files()), len(zt.File))
+	if len(zu.Files()) != len(zt.BaseFile) {
+		t.Fatalf("file count=%d, want %d", len(zu.Files()), len(zt.BaseFile))
+	}
+	for _, ft := range zt.BaseFile {
+		updateReadTestFile(t, zu, &ft)
 	}
 
-	for _, ft := range zt.File {
-		updateReadTestFile(t, zu, ft)
+	b := new(bytes.Buffer)
+	for _, ft := range zt.AppendFile {
+		updateWriteTestFile(t, zu, &ft)
+	}
+	if err := zu.SaveAs(b); err != nil {
+		t.Fatalf("SaveAs error=%v", err)
+	}
+
+	zr, err := NewUpdater(bytes.NewReader(b.Bytes()), int64(b.Len()))
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer zr.Discard()
+	if len(zr.Files()) != len(zt.ResultFile) {
+		t.Fatalf("file count=%d, want %d", len(zu.Files()), len(zt.BaseFile))
+	}
+	for _, ft := range zt.BaseFile {
+		updateReadTestFile(t, zu, &ft)
 	}
 }
 
-func updateReadTestFile(t *testing.T, zu *Updater, ft ZipTestFile) {
+func updateReadTestFile(t *testing.T, zu *Updater, ft *ZipTestFile) {
 	files := zu.Files()
 	index := sort.Search(len(files), func(i int) bool {
 		return files[i].Name() == ft.Name
@@ -109,5 +149,18 @@ func updateReadTestFile(t *testing.T, zu *Updater, ft ZipTestFile) {
 			t.Errorf("%s: content[%d]=%q want %q", ft.Name, i, b, c[i])
 			return
 		}
+	}
+}
+
+func updateWriteTestFile(t *testing.T, zu *Updater, ft *WriteTest) {
+	w, err := zu.Create(ft.Name)
+	if err != nil {
+		t.Fatalf("%s: Create error=%v", ft.Name, err)
+	}
+	defer w.Close()
+
+	_, err = w.Write(ft.Data)
+	if err != nil {
+		t.Fatalf("%s: Write error=%v", ft.Name, err)
 	}
 }
